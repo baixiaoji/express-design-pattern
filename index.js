8f15ff1b4952035b8c49164640916e2dd2265a00
baixiaoji <1041874421@qq.com>
2023-11-28 09:15:22 +0800

    feat: router design pattern - handler 为单函数 - map 管理路由 - 启动逻辑一句话

--- index.js
+++ index.js
@@ -6,22 +6,35 @@ const emails = require("./fixtures/emails.json");
 
 const app = express()
 
+let getUsersRoute = (req, res) => {
+    res.send(users)
+}
+let getEmailsRoute = (req, res) => {
+    res.send(emails)
+}
+
+let noRouteHandler = (req, res) => {
+    let route = req.method + " " + req.url;
+    res.end(`you asked for ${route}\n`);
+}
+
+let routes = {
+    'GET /users': getUsersRoute,
+    'GET /emails': getEmailsRoute,
+}
+
 app.use((req, res) => {
     let route = req.method + " " + req.url;
-  
-    if (route === "GET /users") {
-      console.log(req.accepts())
-      res.type(req.accepts()[0])
-      res.send(users);
-    } else if (route === "GET /emails") {
-      res.send(emails);
-    } else {
-      res.end(`you asked for ${route}\n`);
-    }
-  })
+
+    const handler = routes[route] || noRouteHandler
+
+    console.log(req.accepts())
+    res.type(req.accepts()[0])
+    handler(req, res)
+})
 
 // let server = http.createServer(app);
 
 app.listen(3000, () => {
-  console.log("listen 3000");
+    console.log("listen 3000");
 });
